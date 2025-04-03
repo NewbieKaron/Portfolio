7adc28db9f5961c9ab8091ed95b267a9ee5df4a4
John <john_michael_velasco@ring-group.jp>
2025-04-03 18:00:01 +0800

    Ref #John_1 - Portfolio Navbar - Button

--- page/pages/index.tsx
+++ page/pages/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Button } from 'styles/components/Button';
+import { Navbar } from 'styles/components/Navbar';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <>
+    <Navbar />
+    <div className='flex flex-col items-center justify-center min-h-screen px-4'>
+      
+      <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-center'>
+        Portfolio
+      </h1>
+      <Button />
     </div>
+    </>
   )
 };
 
