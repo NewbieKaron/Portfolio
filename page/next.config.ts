251173e5887056080ceb08bc30950248673b8f97
John <john_michael_velasco@ring-group.jp>
2025-04-03 17:03:55 +0800

    DEFAULT

---
+++ page/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "styles/page/node_modules/next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+export default nextConfig;
