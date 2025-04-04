c485407a86397ed0a10baa27294c0e279fb71a50
John <john_michael_velasco@ring-group.jp>
2025-04-04 14:09:07 +0800

    Ref #John_5 - Added dark/light toggle

--- page/components/Button.tsx
+++ page/components/Button.tsx
@@ -1,15 +1,16 @@
-import Link from "next/link";
 
 import { ButtonLabel } from "@page/enums/page";
 
 export const Button = () => {
     const changeButtonProp = () => {
         console.log("Clicked button");
-        
+        document.body.classList.toggle("dark");
     }
     return (
         <div> 
-            <button className="rounded-[10px] bg-blue-500 mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-8 py-4 cursor-pointer hover:bg-red-500" onClick={changeButtonProp}><Link href='/projects'>{ButtonLabel.INDEX_BUTTON_LABEL}</Link></button>
+            <button className="rounded-[10px] bg-blue-500 mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-8 py-4 cursor-pointer hover:bg-red-500" onClick={changeButtonProp}>{ButtonLabel.INDEX_BUTTON_LABEL}</button> 
         </div>
     )
 }
+
+//<Link href='/projects'> </Link>
