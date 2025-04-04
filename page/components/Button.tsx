b937c492675f422ebbcb74ced9c306a7d161e7a8
John <john_michael_velasco@ring-group.jp>
2025-04-04 13:22:17 +0800

    Ref #John_2 - Added About me page - Project page

--- page/components/Button.tsx
+++ page/components/Button.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
+import { ButtonLabel } from "@page/enums/page";
 
 export const Button = () => {
     const changeButtonProp = () => {
         console.log("Clicked button");
+        
     }
     return (
         <div> 
-            <button className="rounded-[10px] bg-blue-500 mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-8 py-4 cursor-pointer hover:bg-red-500" onClick={changeButtonProp}>Click</button>
+            <button className="rounded-[10px] bg-blue-500 mt-4 sm:mt-6 md:mt-8 lg:mt-10 px-8 py-4 cursor-pointer hover:bg-red-500" onClick={changeButtonProp}><Link href='/projects'>{ButtonLabel.INDEX_BUTTON_LABEL}</Link></button>
         </div>
     )
 }
